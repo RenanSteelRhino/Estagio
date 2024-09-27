22a8d231ed833511ff7d92d0f32807034dd09626
Renan P. Bet <146025607+RenanSteelRhino@users.noreply.github.com>
2024-09-27 16:34:23 -0300

    Nav Mesh 2D

--- IdleEstagio/Assets/_Sripts/_Player/FollowCamera.cs
+++ IdleEstagio/Assets/_Sripts/_Player/FollowCamera.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FollowCamera : MonoBehaviour
 {
@@ -6,7 +7,14 @@ public class FollowCamera : MonoBehaviour
     private Vector2 destination;
     public Vector2 distance;
     public Vector2 variableDistance;
+    public NavMeshAgent agent;
 
+    private void Start() 
+    {
+        agent.updateRotation = false;
+		agent.updateUpAxis = false;
+    }
+    
     private void Update() 
     {
         RaycastHit2D hit = Physics2D.Raycast(Camera.main.transform.position, -Camera.main.transform.up, 15, tileLayer);
@@ -14,11 +22,12 @@ public class FollowCamera : MonoBehaviour
         if(hit)
         {
             destination = hit.point;
+            agent.SetDestination(destination);
 
-            distance = destination - (Vector2)transform.position;
-            variableDistance = new Vector2(distance.x, distance.y + 0.43f) / 500;
+            // distance = destination - (Vector2)transform.position;
+            // variableDistance = new Vector2(distance.x, distance.y + 0.43f) / 500;
 
-            transform.position += (Vector3)variableDistance;
+            // transform.position += (Vector3)variableDistance;
         }
 
     }
