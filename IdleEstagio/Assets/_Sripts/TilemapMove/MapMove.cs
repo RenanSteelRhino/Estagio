c57838f89cc95338f43f793b392e1360a5445bf5
Renan P. Bet <146025607+RenanSteelRhino@users.noreply.github.com>
2024-10-02 16:38:55 -0300

    Parallax effect on the idle Slayer Scene

--- IdleEstagio/Assets/_Sripts/TilemapMove/MapMove.cs
+++ IdleEstagio/Assets/_Sripts/TilemapMove/MapMove.cs
@@ -5,20 +5,47 @@ using UnityEngine;
 public class MapMove : MonoBehaviour
 {
     public float speed = 5f;
-    public float leftBound = -10f;
-    public float initialPositionX = 10f;
+    public List<Renderer> renderes = new List<Renderer>();
+    private Camera _cam;
+    private float spriteSize;
+    public float offset = 0;
+
+    private void Awake() 
+    {
+        _cam = Camera.main;
+    }
+    
+    private void Start() 
+    {
+        if(renderes.Count <= 0) return;
+
+        for (int i = 0; i < renderes.Count; i++)
+        {
+            spriteSize = renderes[i].bounds.extents.x * 2;
+            var xPos = spriteSize * i+1;
+            renderes[i].transform.position = new Vector3(xPos, renderes[i].transform.position.y, renderes[i].transform.position.z);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
-
-        if (transform.position.x <= leftBound)
+        for (int i = 0; i < renderes.Count; i++)
         {
-            ResetPositionX();
+            renderes[i].transform.Translate(Vector2.left * speed * Time.deltaTime);
+            ResetPositionX(renderes[i]);
         }
+            
     }
-    void ResetPositionX()
+    void ResetPositionX(Renderer render)
     {
-        transform.position = new Vector3(initialPositionX, transform.position.y, transform.position.z);
+        var cameraBoundsX = _cam.GetComponentInChildren<SpriteRenderer>().bounds.extents.x;
+        var distanceToReset = -(spriteSize/2) - cameraBoundsX - offset;
+
+        if(render.transform.position.x <= distanceToReset)
+        {
+            render.transform.position = new Vector3(render.transform.position.x + spriteSize * 3, transform.position.y, transform.position.z);
+        }
+
     }
 }
