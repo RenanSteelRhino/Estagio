4b8910379fbbd0a7268e7aec60f3a3e3d3ebeff8
Renan P. Bet <146025607+RenanSteelRhino@users.noreply.github.com>
2024-10-02 16:39:49 -0300

    Player jump Script

---
+++ IdleEstagio/Assets/_Sripts/_Player/PlayerJump.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown() 
+    {
+        //PULA NO CLICK
+    }
+}
